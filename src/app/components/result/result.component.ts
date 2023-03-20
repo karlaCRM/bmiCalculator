16e53539c7106baac49250c9610d537181f133f0
karlaCRM <abraxaskrm@gmail.com>
2023-03-19 18:40:49 -0600

    last corrections html

--- src/app/components/result/result.component.ts
+++ src/app/components/result/result.component.ts
@@ -25,19 +25,19 @@ getResult(){
   if(this.bim >= 25){
     this.result = `Overweight`;
     this.interpretation= `You are overweight, don't worry too much. Try to exercise and eat healthy.`
-  }
- if(this.bim >= 18.5 && this.bim < 30){
+  };
+  if(this.bim >= 18.5 && this.bim < 30){
     this.result = `Normal`;
     this.interpretation= `You have a normal weight. It's still important to exercise.`
-  }
+  };
   if(this.bim >= 30 && this.bim < 35 ){
     this.result = `Obesity`;
     this.interpretation=`You have obesity. It is important that you know that it is not synonymous with being sick.`
-  }
+  };
   if(this.bim >= 35 ){
     this.result = `Class II Obesity`;
     this.interpretation=`You have class II obesity. It is important that you know that it is not synonymous with being sick.`
-  }
+  };
  if(this.bim <= 18.5 ){
     this.result = `Underweight`;
     this.interpretation=`You have a lower body weight than normal. It is not synonymous with being sick.`
